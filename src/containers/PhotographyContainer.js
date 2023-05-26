86d0759d7568e73d8809a6a2bd6c3a5d6ff1799b
Jennifer <jng-96@hotmail.co.uk>
2023-05-26 13:52:15 +0100

    container set up and displaying correctly

---
+++ src/containers/PhotographyContainer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+import React, { useState, useEffect } from 'react';
+
+const PhotographyContainer = () => {
+
+    return (
+        <h1>This is the photography container</h1>
+    )
+}
+
+export default PhotographyContainer 
